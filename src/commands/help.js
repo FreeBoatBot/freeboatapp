11b2091f77e16a76d7e70ec7474de9f402f941e5
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-03-09 07:04:31 +0700

    Create help.js

---
+++ src/commands/help.js
@@ -0,0 +1,9 @@
+const response = require('../../../resp')
+require('./message')
+response.AddMessage(function static() =>{
+.run('Read this https://github.com/FreeBoatBot/freeboatapp/wiki!')
+.static('http://static.freeboat.com/cmd/help.js')
+.resfile('../../class')
+
+
+});
